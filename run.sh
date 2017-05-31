dcb097bc1e99eeb32cd267394455d9fbe53c7f3b
wsjeon <wonsjeon@kaist.ac.kr>
2017-06-01 00:18:04 +0900

    Now works on CPUs

--- run.sh
+++ run.sh
@@ -7,8 +7,7 @@ tmux new-window -t examplesess -n worker-1 -d bash
 sleep 1
 tmux send-keys -t examplesess:ps-0 'CUDA_VISIBLE_DEVICES= python example.py --job_name ps --task_index 0' Enter
 tmux send-keys -t examplesess:ps-1 'CUDA_VISIBLE_DEVICES= python example.py --job_name ps --task_index 1' Enter
-tmux send-keys -t examplesess:worker-1 'CUDA_VISIBLE_DEVICES=0 python example.py --job_name worker --task_index 1' Enter
-sleep 1
 tmux send-keys -t examplesess:worker-0 'CUDA_VISIBLE_DEVICES=0 python example.py --job_name worker --task_index 0' Enter
+tmux send-keys -t examplesess:worker-1 'CUDA_VISIBLE_DEVICES=0 python example.py --job_name worker --task_index 1' Enter
 sleep 1
 tmux a
